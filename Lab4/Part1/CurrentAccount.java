b79652b74a449f7733cf36e3524c274f0c32d569
Michal Kornacki <mkornacki888@gmail.com>
2023-10-23 20:27:30 +0100

    removed unneccesary comments

--- Lab4/Part1/CurrentAccount.java
+++ Lab4/Part1/CurrentAccount.java
@@ -3,34 +3,25 @@ import java.time.LocalDate;
 
 public class CurrentAccount extends BankAccount{
     private double overDraftLimit=200;//expressed as number of dollars, default overdraft limit is set to 200
-    //private double balance;
     private LocalDate dateCreated = getDateCreated();
-    //private int id;
     private String accountName;
     
 
     //this makes overDraftLimit set to default which is 200 dollars
     public CurrentAccount(int id,double balance){
         super(id,balance);   
-        //this.id=id;
-        //this.balance=balance;
-
     }
 
     
 
     public CurrentAccount(double balance, int id,String accountName){
         super(id,balance);
-        //this.id=id;
-        //this.balance=balance;
         this.accountName=accountName;
     }
 
     //can set a custom overdraft limit
     public CurrentAccount(double balance, int id, String accountName,double overDraftLimit){
         super(id,balance);
-        //this.id=id;
-        //this.balance=balance;
         this.overDraftLimit = overDraftLimit;
         this.accountName = accountName;
     }
--- Lab4/Part1/SavingsAccount.java
+++ Lab4/Part1/SavingsAccount.java
@@ -3,23 +3,16 @@ package Lab4.Part1;
 import java.time.LocalDate;
 
 public class SavingsAccount extends BankAccount{
-    //private double balance;
     private double annualInterestRate = getAnnualInterestRate();
     private LocalDate dateCreated = LocalDate.now();//Uses LocalDate.now becuase a savings account doesn't necessarily have to be created at the same time as a current account
-    //private int id;
     private String accountName;
-    
 
     public SavingsAccount(double balance, int id){
         super(id,balance);
-        //this.balance = balance;
-        //this.id = id;
     }
 
     public SavingsAccount(double balance, int id,String accountName){
         super(id,balance);
-        //this.balance = balance;
-        //this.id = id;
         this.accountName = accountName;
     }
 
